46a78ff2bc5363d297cb58af6c2e6f0c8b0d25e1
Matt Leon <matth@MatthewsSurface.localdomain>
2018-04-26 12:41:35 -0500

    Make cachemap use sync.Map

--- cache/cache.go
+++ cache/cache.go
@@ -13,38 +13,29 @@ import (
 const defaultCache = "cache.txt"
 
 var (
-	mu          = sync.RWMutex{}
-	cache       map[string]string
+	cache       sync.Map
 	cachename   string
 	initialized = false
 )
 
 // Set caches a value in the map and writes it to disk
 func Set(key string, value string) error {
-	mu.Lock()
-	defer mu.Unlock()
-
 	if !initialized {
 		err := Load(defaultCache)
 		if err != nil {
-			log.Printf("Looks like the default cache doesn't exist: %s", err)
+			log.Fatalf("Error loading cache: %s", err)
 		}
 	}
 
 	append(key, value)
 
-	if _, ok := cache[key]; !ok {
-		cache[key] = value
-	}
+	cache.LoadOrStore(key, value)
 
 	return nil
 }
 
 // Get returns a value from the map
 func Get(key string) (string, error) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	if !initialized {
 		err := Load(defaultCache)
 		if err != nil {
@@ -52,8 +43,8 @@ func Get(key string) (string, error) {
 		}
 	}
 
-	if v, ok := cache[key]; ok {
-		return v, nil
+	if v, ok := cache.Load(key); ok {
+		return v.(string), nil
 	}
 
 	return "", fmt.Errorf("Key not found")
@@ -61,25 +52,10 @@ func Get(key string) (string, error) {
 
 // Load initializes the in-memory map with the information from the disk cache
 func Load(filename string) error {
-	mu.Lock()
-	defer mu.Unlock()
-
 	cachename = filename
-	cache = make(map[string]string)
 	initialized = true
 
-	// Check if file exists
-	if _, err := os.Stat(filename); err != nil {
-		if os.IsNotExist(err) {
-			os.Create(filename)
-			log.Printf("Cache file not found, created file '%s'", filename)
-		} else {
-			return err
-		}
-	}
-
-	// Open file
-	f, err := os.Open(filename)
+	f, err := os.OpenFile(cachename, os.O_CREATE|os.O_RDONLY, 0644)
 	defer f.Close()
 	if err != nil {
 		return err
@@ -90,12 +66,12 @@ func Load(filename string) error {
 	// Load each line into map
 	for scanner.Scan() {
 		line := scanner.Text()
-
 		val := strings.Split(line, "_")
+
 		// Load into map
 		if len(val) == 2 {
-			if _, ok := cache[val[0]]; !ok {
-				cache[val[0]] = val[1]
+			if _, loaded := cache.LoadOrStore(val[0], val[1]); loaded {
+				log.Printf("Duplicate value in cache for key '%s'", val[0])
 			}
 		}
 	}
@@ -105,25 +81,22 @@ func Load(filename string) error {
 
 // Export writes a new disk cache file
 func Export(filename string) error {
-	mu.Lock()
-	defer mu.Unlock()
-
 	cachename = filename
-	f, err := os.Create(filename)
+
+	f, err := os.OpenFile(cachename, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
 	if err != nil {
 		return err
 	}
-	defer f.Close()
 
-	// Initialize map if it hasn't been
-	if !initialized {
-		cache = make(map[string]string)
-		initialized = true
-	}
+	cache.Range(func(k interface{}, v interface{}) bool {
+		_, err = fmt.Fprintf(f, "%s_%s\n", k.(string), v.(string))
+		if err != nil {
+			log.Fatalf("Error writing cache file: '%s'", err)
+		}
 
-	for k, v := range cache {
-		_, err = fmt.Fprintf(f, "%s_%s\n", k, v)
-	}
+		return true
+	})
 
 	return nil
 }
