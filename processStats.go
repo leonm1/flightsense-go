c27199f014b5548e5af3eeab62bf9d9c5f79b111
Matt Leon <matth@MatthewsSurface.localdomain>
2018-04-27 00:25:49 -0500

    Add parseArguments to main file

--- processStats.go
+++ processStats.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -332,4 +335,85 @@ func (f *Flight) toSlice() *[]string {
 	ret[18] = strconv.FormatFloat(f.PrecipIntensityDest, 'f', -1, 64)
 
 	return &ret
+}
+
+func parseArguments() (*[]string, *[]string, *string) {
+	var (
+		files     []string
+		filenames []string
+		outPath   string
+	)
+
+	// Parse command-line flags for input and output files
+	inname := flag.String("in", "", "Optional: Input file name (Cycles through directory if ommitted)")
+	infolder := flag.String("indir", "", "Directory of source data files")
+	outFolder := flag.String("outdir", "", "Directory of destination data files")
+	flag.Parse()
+
+	if *inname == "" && *infolder == "" {
+		log.Fatalf("Input arguments requrired!")
+		os.Exit(1)
+	}
+
+	if strings.Contains(*inname, "/") && *infolder == "" {
+		s := strings.Split(*inname, "/")
+
+		// Set filename as inname
+		*inname = s[len(s)-1]
+
+		// Remove filename
+		s = s[:len(s)-1]
+		*infolder = strings.Join(s, "/")
+		log.Printf("*infolder is now '%s'", *infolder)
+	}
+
+	if !strings.HasSuffix(*outFolder, "/") {
+		if *outFolder == "" {
+			outPath = "./"
+		} else {
+			outPath = *outFolder + "/"
+		}
+	}
+
+	// Read all csv files in indir, skipping subdirectories
+	filepath.Walk(*infolder, func(path string, f os.FileInfo, _ error) error {
+		if f.IsDir() && path != *infolder {
+			log.Printf("Skipping dir \"%s\"", f.Name())
+			// Skip subdirectories
+			return filepath.SkipDir
+		} else if *inname != "" {
+			if f.Name() == *inname {
+				files = append(files, path)
+				filenames = append(filenames, f.Name())
+			}
+		} else if filepath.Ext(path) == ".csv" {
+			files = append(files, path)
+			filenames = append(filenames, f.Name())
+		}
+
+		return nil
+	})
+
+	// Check to ensure input files exist
+	for _, v := range files {
+		if _, err := os.Stat(v); err != nil {
+			if os.IsNotExist(err) {
+				log.Fatalf("Error 404 - File not found: \"%s\".\nHere's the error: %s", v, err)
+			}
+		} else {
+			log.Printf("Found file \"%s\"", v)
+		}
+	}
+
+	// Check if outdir exists
+	if _, err := os.Stat(outPath); err != nil {
+		if os.IsNotExist(err) {
+			os.Create(outPath)
+			log.Printf("Output directory not found, created '%s'", outPath)
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	return &files, &filenames, &outPath
 }
